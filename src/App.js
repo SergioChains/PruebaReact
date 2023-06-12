8cb602b35a34688d564ff9d458d2b809aa6e2b51
Sergio Cadenas González <cadenassergio53@gmail.com>
2023-06-12 17:26:20 -0500

    Se realizo section-four, section-five y footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React, { useState, useEffect } from 'react';
 import Image1 from './images/imagen1.jpg';
 import Image2 from './images/imagen2.jpg';
 import Image3 from './images/imagen3.jpg';
+// import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const images = [
   { src: Image1, alt: 'Fotografía 1' },
@@ -75,7 +76,7 @@ function App() {
 
       <div className="Section-One">
       <div className="bg-black flex align-center justify-center items-center h-screen border-white">
-          <div className="flex w-auto bg-white">
+          <div className="flex w-auto bg-black">
             <figure className="w-1/2 content-center">
             <img
               src={images[currentImage].src}
@@ -185,22 +186,76 @@ function App() {
       </body>
     </div>
     <div className="Section-Five">
-      <body>
-          <h1>Said y Mau me la jalan</h1>
-      </body>
+    
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d657.8364279490553!2d-86.83550633444858!3d21.14106167821356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f4c2bbcccf5f36f%3A0x9292707424b50383!2sKumo%20Motors%20Seminuevos!5e0!3m2!1ses!2smx!4v1686599551497!5m2!1ses!2smx" width="1500" height="600" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+     
     </div>
 
-  <footer className='Foot'>
-    <h2>
-      Este es para el footer
-    </h2>
-  </footer>
-</div>
+    <footer className="site-footer">
+    
+        <div class="row">
+    
+          <div class="col-xs-6 col-md-3">
+            <h6>Contacto</h6>
+            <ul class="footer-links">
+              <li><a href="https://api.whatsapp.com/send?phone=%2B529988807458&data=ARDBBY6AKTZiCjnexUpEKk-Tis_khlkk1mc6IKV_w0KXjUOLTcXUKKbjY5yMaBUXqVhFPz1bj-oyg97kuTUbTTTvQmXF5DBzU--q08clS5DDxHNzv-Zl75PRRMSIvOUBJtx7K1RxNZ-VJTNekZiRRrMx6Q&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR0XpXYQgTtFLHP5CM2qyI3lyAFKCHafei0LcgaM-iyyIhMYtCH4FOxcPQk">9988807458</a></li>
+              <li><a href="mailto:kumomotors_ventas@hotmail.com?subject=Contactar&body=Buenas%20tardes%2C%20quisiera%20ponerme%20en%20contacto%20para%20obtener%20mas%20informaci%C3%B3n.%20">Correo Electrónico </a></li>
+       
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Redes Sociales</h6>
+            <ul class="footer-links">
+              <li><a href="https://www.facebook.com/KumoMotors">Facebook</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr></hr>
+    
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2023 All Rights Reserved by :
+         <a href="#"> KumoMotors</a>.
+            </p>
+          </div>
+          
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="https://www.facebook.com/KumoMotors"><i class="fab fa-facebook"></i></a></li>
+              <li><a class="whatsapp" href="https://api.whatsapp.com/send?phone=%2B529988807458&data=ARDBBY6AKTZiCjnexUpEKk-Tis_khlkk1mc6IKV_w0KXjUOLTcXUKKbjY5yMaBUXqVhFPz1bj-oyg97kuTUbTTTvQmXF5DBzU--q08clS5DDxHNzv-Zl75PRRMSIvOUBJtx7K1RxNZ-VJTNekZiRRrMx6Q&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR0XpXYQgTtFLHP5CM2qyI3lyAFKCHafei0LcgaM-iyyIhMYtCH4FOxcPQk"><i class="fab fa-WhatsApp"></i></a></li></ul>
+          </div>
+        </div>
+      
+</footer>
+    </div>
  );
    }
 
 
 
+
+  //  <footer className="bg-gray-900 py-8">
+  //     <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+  //       <div className="text-white mb-4 md:mb-0">
+  //         <h4 className="text-lg font-bold">Contacto</h4>
+  //         <p>Email: ejemplo@example.com</p>
+  //         <p>Teléfono: 123-456-7890</p>
+  //         <p>Dirección: Calle Principal, Ciudad</p>
+  //       </div>
+  //       <div className="flex space-x-4">
+  //         <a href="#" className="text-white hover:text-gray-300">
+  //           <FaFacebook size={24} />
+  //         </a>
+  //         <a href="#" className="text-white hover:text-gray-300">
+  //           <FaTwitter size={24} />
+  //         </a>
+  //         <a href="#" className="text-white hover:text-gray-300">
+  //           <FaInstagram size={24} />
+  //         </a>
+  //       </div>
+  //     </div>
+  //   </footer>
    
 
 export default App;
